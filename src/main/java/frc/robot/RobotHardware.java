fc6f79c90c58b7f433c203f37a3af6e9d25efea8
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-12-20 09:32:48 -0500

    added comments

--- src/main/java/frc/robot/RobotHardware.java
+++ src/main/java/frc/robot/RobotHardware.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+/**
+ * This is were you Define all physical robot hardware (IE: motor controllers,
+ * encoders, Gryos , etc) Template by Nick B on the FRC 5740 team
+ */
+
 public class RobotHardware {
 
 }

1500cb42b56ba30625a2567b0adc199055439c31
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-12-20 09:39:40 -0500

    added Base robot map

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,10 +7,19 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+  // object for calling robot map class
+  RobotMap map;
+
+  // Driver Station Controlers
+  XboxController Driver = new XboxController(map.DSDriver);
+  XboxController Operator = new XboxController(map.DSOperator);
 
 }
--- src/main/java/frc/robot/RobotHardware.java
+++ src/main/java/frc/robot/RobotHardware.java
@@ -1,10 +1,16 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Spark;
+
 /**
  * This is were you Define all physical robot hardware (IE: motor controllers,
  * encoders, Gryos , etc) Template by Nick B on the FRC 5740 team
  */
 
 public class RobotHardware {
+
+    RobotMap map;
+
+    Spark frontLSpark = new Spark();
 
 }

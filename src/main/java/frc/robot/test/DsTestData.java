e239fb44b896e5af4bef379a203df1054b75d6c2
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-12-22 09:14:55 -0500

    fixing class

--- src/main/java/frc/robot/test/DsTestData.java
+++ src/main/java/frc/robot/test/DsTestData.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.OI;
 import frc.robot.RobotMap;
+import frc.robot.subsystems.CameraConfig;
 
 /**
  * This Class is For Displaying Data From the Robot to ShuffleBoard in Test Mode
@@ -16,17 +18,20 @@ import frc.robot.RobotMap;
  */
 public class DsTestData {
 
+        static double text;
+
         // Creates an Object for shuffleboard and creates a tab named DATA
         public static ShuffleboardTab tab = Shuffleboard.getTab("DATA");
 
-        // Uses NetWorktables to pass Data to Shuffleboard
-        private final NetworkTableEntry ControllerX = tab.add("ControllerX", OI.Driver.getRawAxis(RobotMap.RStick))
-                        .withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 2).withSize(2, 1).getEntry();
+        // Created an Complex widget that displays video feed
+        private final ComplexWidget frontCameraWidget = tab.add("Normal_Video", CameraConfig.CvCamera)
+                        .withPosition(7, 0).withSize(3, 4).withWidget(BuiltInWidgets.kCameraStream);
 
-        // Created a simple Display widget to Display controller 1 axis
-        private final NetworkTableEntry ControllerY = tab.add("ControllerY", OI.Driver.getRawAxis(RobotMap.LStick))
-                        .withWidget(BuiltInWidgets.kNumberBar).withPosition(0, 4).withSize(2, 1).getEntry();
+        // Creates an Network Table entry to Controller info
+        private final NetworkTableEntry ControllerX = tab.add("ControllerX", text).withPosition(0, 2).withSize(2, 1)
+                        .withWidget(BuiltInWidgets.kNumberBar).getEntry();
 
+        private final
         // Function to constally update Shuffleboard Values
         public final static void perodic() {
                 /*
